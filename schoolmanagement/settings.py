597cb6865b5232c36521d5e39a3e17707b2001b8
Sanjar0601 <105801077+Sanjar0601@users.noreply.github.com>
2024-10-04 04:13:17 +0500

    Update settings.py
    saas

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -16,7 +16,6 @@ from .juzmin import JAZZMIN_SETTINGS
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATABASE_URL = "postgresql://postgres:UHqCJKcOFKqKAPGPFbQxrZExJYIxrcgb@postgres.railway.internal:5432/railway"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
