87a981ad5762af10197e6906843a3800182f1683
unknown <sanjarshukurov0601@gmail.com>
2024-10-06 13:34:48 +0500

    new vps settings configured

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -95,12 +95,16 @@ WSGI_APPLICATION = 'schoolmanagement.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=env('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env("DB_PORT"),
 
-    )
+    }
+
 }
 
 
