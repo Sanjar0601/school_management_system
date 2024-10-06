ebf10c2ff1161fe8c415bc7d8f963fe9faab3ae5
unknown <sanjarshukurov0601@gmail.com>
2024-10-06 19:58:58 +0500

    environ deleted

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -11,12 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import environ
-import dj_database_url
 from .juzmin import JAZZMIN_SETTINGS
-
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
