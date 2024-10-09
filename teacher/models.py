d36dea9d22a9a63540d5a1e9453a9f381c032ee0
unknown <sanjarshukurov0601@gmail.com>
2024-10-09 10:35:54 +0500

    blank true null true added

--- teacher/models.py
+++ teacher/models.py
@@ -4,8 +4,8 @@ from administration.models import Designation
 
 class PersonalInfo(models.Model):
     name = models.CharField(max_length=45, )
-    phone_no = models.CharField(max_length=11, unique=True)
-    email = models.CharField(max_length=255, unique=True)
+    phone_no = models.CharField(max_length=11, blank=True, null=True)
+    email = models.CharField(max_length=255, blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
     tenant = models.ForeignKey('account.Tenant', on_delete=models.SET_NULL, null=True, related_name='tenants')
 
