01d64516633dfd1ac25bd333a7f0ee362ac770e8
unknown <sanjarshukurov0601@gmail.com>
2025-01-02 21:46:15 +0500

    values for attendance removed extra

--- student/views.py
+++ student/views.py
@@ -340,7 +340,7 @@ MESSAGE_TEMPLATES = {
     "Absent": "Assalomu alaykum! Farzandingiz {name} bugungi  {group} darsga kelmadi.",
     "0": "Assalomu alaykum! Farzandingiz {name} bugungi  {group} darsga kelmadi.",
     "1": "Dear {name}, your absence was EXCUSED today in {group}.",
-    "2": "Dear {name}, you were UNINFORMED about today's session in {group}.",
+    "2": "Assalomu alaykum! Farzandingiz {name} bugun darsga tayyorlanib kelmadi, ko`proq e'tibor ajratishingizni so`raymiz",
     "3": "Assalomu alaykum! Farzandingizning bugungi darslardagi harakatlari qoniqarsiz, ko'proq e'tibor ajratilishini so'raymiz.",
     "4": "Assalomu alaykum! {name}ning darsdagi faol harakatlari e’tiborga olindi. Ammo yanada yaxshiroq harakat qilish maqsadga muvofiq bo’lar edi.",
     "5": "Assalomu alaykum! {name} bugungi {group} darsga to'liq tayyor kelib, dars jarayonida faol qatnashganligi va o'zlashtirish darajasi a'lo ekanligini ma'lum qilamiz.",
