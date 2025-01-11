b89d349678ab39cc820b4c10120d55c3d0fc7469
unknown <sanjarshukurov0601@gmail.com>
2025-01-11 19:39:32 +0500

    exam status added

--- student/message_templates.py
+++ student/message_templates.py
@@ -1,11 +1,13 @@
 MESSAGE_TEMPLATES = {
-    "Absent": "Assalomu alaykum! Farzandingiz {name} bugungi  {group} darsga kelmadi.",
+    "Absent": "Assalomu alaykum! Farzandingiz {name} bugungi  {group} darsga kelmadi. POLYGLOT LANGUAGE SCHOOL",
     "Qarz": "Assalomu aleykum, hurmatli {name} sizni {group} kursi bo'yicha 699.000 so'm qarzdorligingiz mavjud, to'lovlarni o'z vaqtida amalga oshirishingizni so'rab qolamiz! POLYGLOT LANGUAGE SCHOOL",
-    "0": "Assalomu alaykum! Farzandingiz {name} bugungi  {group} darsga kelmadi.",
-    "1": "Dear {name}, your absence was EXCUSED today in {group}.",
-    "2": "Assalomu alaykum! Farzandingiz {name} bugun darsga tayyorlanib kelmadi, ko`proq e'tibor ajratishingizni so`raymiz",
-    "3": "Assalomu alaykum! Farzandingizning bugungi darslardagi harakatlari qoniqarsiz, ko'proq e'tibor ajratilishini so'raymiz.",
-    "4": "Assalomu alaykum! {name}ning darsdagi faol harakatlari e’tiborga olindi. Ammo yanada yaxshiroq harakat qilish maqsadga muvofiq bo’lar edi.",
-    "5": "Assalomu alaykum! {name} bugungi {group} darsga to'liq tayyor kelib, dars jarayonida faol qatnashganligi va o'zlashtirish darajasi a'lo ekanligini ma'lum qilamiz.",
-    "Choice": ''
+    "0": "Assalomu alaykum! Farzandingiz {name} bugungi  {group} darsga kelmadi. POLYGLOT LANGUAGE SCHOOL",
+    "1": "Dear {name}, your absence was EXCUSED today in {group}. POLYGLOT LANGUAGE SCHOOL",
+    "2": "Assalomu alaykum! Farzandingiz {name} bugun darsga tayyorlanib kelmadi, ko`proq e'tibor ajratishingizni so`raymiz. POLYGLOT LANGUAGE SCHOOL",
+    "3": "Assalomu alaykum! Farzandingizning bugungi darslardagi harakatlari qoniqarsiz, ko'proq e'tibor ajratilishini so'raymiz. POLYGLOT LANGUAGE SCHOOL",
+    "4": "Assalomu alaykum! {name}ning darsdagi faol harakatlari e’tiborga olindi. Ammo yanada yaxshiroq harakat qilish maqsadga muvofiq bo’lar edi. POLYGLOT LANGUAGE SCHOOL",
+    "5": "Assalomu alaykum! {name} bugungi {group} darsga to'liq tayyor kelib, dars jarayonida faol qatnashganligi va o'zlashtirish darajasi a'lo ekanligini ma'lum qilamiz. POLYGLOT LANGUAGE SCHOOL",
+    "Choice": '',
+    'Exam': 'Assalomu aleykum, farzandingiz {name} {group} kursida imtihon topshirmoqda! POLYGLOT LANGUAGE SCHOOL.'
+
     }
